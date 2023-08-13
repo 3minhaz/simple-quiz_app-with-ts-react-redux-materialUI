7dbb75f2770ea0a28bd5b4c22aed562a3f3387d7
3minhaz <3minhaz@gmail.com>
2023-08-13 14:48:59 +0600

    remove unnecessary comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,11 @@
-// import './App.css'
-// import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import QuestionCard from "./components/QuestionCard";
-// import { useAppSelector } from "./redux/hook";
-// import React from "react";
-// import { Modal } from "@mui/material";
-// import CustomizedDialogs from "./components/modal";
 
 function App() {
-  // const testValue = useAppSelector((state) => state.quiz);
-  // console.log(testValue);
-  // const [open, setOpen] = React.useState(false);
-  // const handleClickOpen = () => {
-  //   setOpen(true);
-  // };
-  // const handleClose = () => {
-  //   setOpen(false);
-  // };
   return (
     <>
       <Typography variant="h3">Quiz App</Typography>
-      {/* <Button variant="contained">Start</Button> */}
-
       <QuestionCard></QuestionCard>
-      {/* <CustomizedDialogs></CustomizedDialogs> */}
     </>
   );
 }
